0cd8b02efc6843faeac0d81a49faa26c10fb1593
raulgrageranogales-eng <raul.gragera.nogales@hotmail.com>
2026-08-27 19:59:04 +0200

    Refactor service worker to use FILES array

--- service-worker.js
+++ service-worker.js
@@ -1,69 +1,123 @@
 const CACHE_NAME = "finanzas-hogar-v1";
 
-const FILES_TO_CACHE = [
-    "./",
-    "./index.html",
-    "./styles.css",
-    "./app.js",
-    "./manifest.json"
+const FILES = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json"
 ];
 
 
-self.addEventListener("install", event => {
+self.addEventListener(
+  "install",
+  event => {
 
     event.waitUntil(
 
-        caches.open(CACHE_NAME)
-            .then(cache => {
+      caches
+        .open(CACHE_NAME)
+        .then(cache =>
+          cache.addAll(FILES)
+        )
 
-                return cache.addAll(
-                    FILES_TO_CACHE
-                );
-
-            })
+        .then(() =>
+          self.skipWaiting()
+        )
 
     );
 
-});
+  }
+);
 
 
-self.addEventListener("activate", event => {
+self.addEventListener(
+  "activate",
+  event => {
 
     event.waitUntil(
 
-        caches.keys().then(keys => {
+      caches.keys()
+        .then(keys =>
 
-            return Promise.all(
+          Promise.all(
 
-                keys
-                    .filter(key =>
-                        key !== CACHE_NAME
-                    )
-                    .map(key =>
-                        caches.delete(key)
-                    )
+            keys
+              .filter(
+                key =>
+                  key !== CACHE_NAME
+              )
 
+              .map(
+                key =>
+                  caches.delete(key)
+              )
+
+          )
+
+        )
+
+        .then(() =>
+          self.clients.claim()
+        )
+
+    );
+
+  }
+);
+
+
+self.addEventListener(
+  "fetch",
+  event => {
+
+    if (
+      event.request.method !== "GET"
+    ) {
+      return;
+    }
+
+
+    event.respondWith(
+
+      caches
+        .match(event.request)
+
+        .then(cached => {
+
+          if (cached) {
+            return cached;
+          }
+
+          return fetch(event.request)
+
+            .then(response => {
+
+              const copy =
+                response.clone();
+
+              caches
+                .open(CACHE_NAME)
+                .then(cache =>
+                  cache.put(
+                    event.request,
+                    copy
+                  )
+                );
+
+              return response;
+
+            })
+
+            .catch(() =>
+              caches.match(
+                "./index.html"
+              )
             );
 
         })
 
     );
 
-});
-
-
-self.addEventListener("fetch", event => {
-
-    event.respondWith(
-
-        caches.match(event.request)
-            .then(cached => {
-
-                return cached ||
-                    fetch(event.request);
-
-            })
-
-    );
-
-});
+  }
+);
