26aad10f30b60bb6bfef4dae0af5797a69750a31
raulgrageranogales-eng <raul.gragera.nogales@hotmail.com>
2026-08-27 20:53:14 +0200

    Refactor cache handling in service worker

--- sw.js
+++ sw.js
@@ -10,23 +10,35 @@ const FILES = [
 ];
 
 self.addEventListener("install", event => {
+
   event.waitUntil(
-    caches.open(CACHE_NAME)
+
+    caches
+      .open(CACHE_NAME)
       .then(cache => cache.addAll(FILES))
       .then(() => self.skipWaiting())
+
   );
+
 });
 
 self.addEventListener("activate", event => {
+
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
+
+    caches
+      .keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+        )
       )
-    ).then(() => self.clients.claim())
+      .then(() => self.clients.claim())
+
   );
+
 });
 
 self.addEventListener("fetch", event => {
@@ -42,6 +54,7 @@ self.addEventListener("fetch", event => {
   }
 
   event.respondWith(
+
     caches.match(event.request)
       .then(cached => {
 
@@ -54,12 +67,21 @@ self.addEventListener("fetch", event => {
 
             const copy = response.clone();
 
-            caches.open(CACHE_NAME)
-              .then(cache => cache.put(event.request, copy));
+            caches
+              .open(CACHE_NAME)
+              .then(cache =>
+                cache.put(event.request, copy)
+              );
 
             return response;
+
           })
-          .catch(() => caches.match("./index.html"));
+          .catch(() =>
+            caches.match("./index.html")
+          );
+
       })
+
   );
+
 });
