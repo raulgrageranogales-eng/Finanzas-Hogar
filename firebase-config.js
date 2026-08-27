2423f7651b6d13b6c1e4d359d234dd4aaf51d115
raulgrageranogales-eng <raul.gragera.nogales@hotmail.com>
2026-08-27 20:43:49 +0200

    Add Firebase configuration to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA1oKOXWYqauiGL4N8Oh3mG3JMP5ZFCxGw",
+  authDomain: "finanzas-hogar-803fd.firebaseapp.com",
+  projectId: "finanzas-hogar-803fd",
+  storageBucket: "finanzas-hogar-803fd.firebasestorage.app",
+  messagingSenderId: "461089916272",
+  appId: "1:461089916272:web:c0755eddab52ea08673bb5"
+};
